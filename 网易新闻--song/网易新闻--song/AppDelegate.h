bcde8f9d62a9f644fbef417bce8a91643dad63f7
宋海保 <songhaibao@songhaibaodemacbook-pro.local>
2016-03-13 19:55:53 +0800

    初始化项目

---
+++ 网易新闻--song/网易新闻--song/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻--song
+//
+//  Created by 宋海保 on 16/3/13.
+//  Copyright © 2016年 宋海保. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻--song/网易新闻--song/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻--song
+//
+//  Created by 宋海保 on 16/3/13.
+//  Copyright © 2016年 宋海保. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
